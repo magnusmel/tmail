899a940715817f6e9bbf0848b0f0b3e8a134e0c0
maggi <maggi@linux-xleg.suse>
2017-08-13 14:11:28 +0200

    First tmail commit - Added UI

---
+++ app/sidebar/sidebar-routes.config.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.ROUTES = [
+    { path: 'dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
+    { path: 'table', title: 'Campaigns', icon: 'library_books', class: '' },
+    { path: 'user', title: 'Lists', icon: 'content_paste', class: '' },
+    { path: 'typography', title: 'Contacts', icon: 'person', class: '' },
+    { path: 'icons', title: 'Statistics', icon: 'timeline', class: '' },
+    { path: 'maps', title: 'Settings', icon: 'location_on', class: '' },
+    { path: 'notifications', title: 'Notifications', icon: 'notifications', class: '' },
+];
+//# sourceMappingURL=sidebar-routes.config.js.map
